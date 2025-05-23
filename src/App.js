d63e7135a04e6f3476cbd7d1fbb50a5235676c15
bharti-kajal <kajalbharti990@gmail.com>
2025-05-23 10:23:38 +0530

    style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import SeaFreightPage from './components/SeaFreight/SeaFreightPage';
-
+import './style.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+// import "../../src/style.css";
 function App() {
   const browserRouter = createBrowserRouter([
     { path: "/", element: <SeaFreightPage /> }
